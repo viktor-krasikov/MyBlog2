242788dbd812d759f0a2105f42f7e29a28a6d8a0
Viktor Krasikov <viktor.krasikov@gmail.com>
2025-03-17 01:49:52 +0800

    Rename index.html to home.html and move to blog/ directory

--- blog/urls.py
+++ blog/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
+
+from django.urls import path
+from .views import home, about
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
+    path('', home, name='home'),
+    path('about/', about, name='about'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'blog/home.html')
+
+
+def about(request):
+    return render(request, 'blog/about.html')
