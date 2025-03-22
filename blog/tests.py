ed829ae9fdda4459e863e9be6a78f489cc1c93d5
Viktor Krasikov <viktor.krasikov@gmail.com>
2025-03-22 20:08:05 +0800

    Add test (1st version)

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from django.urls import reverse
+from . models import Post
 
-# Create your tests here.
+class UrlsTests(TestCase):
+    def test_home_url(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Добро пожаловать в мой блог!")  # Проверяем наличие текста
+
+    def test_about_url(self):
+        response = self.client.get(reverse('about'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_detail_url(self):
+        response = self.client.get(reverse('post_detail', args=[1]))  # Тестируем маршрут поста с ID 1
+        self.assertEqual(response.status_code, 200)
+
+    def test_invalid_url(self):
+        response = self.client.get('/nonexistent-url/')
+        self.assertEqual(response.status_code, 404)
+
+
+class FormTests(TestCase):
+    def test_create_post_form(self):
+        response = self.client.post(reverse('posts/create/'), {
+            'title': 'New Post',
+            'content': 'New content',
+        })
+        self.assertEqual(response.status_code, 302)  # Проверяем, что происходит перенаправление
+        self.assertTrue(Post.objects.filter(title='New Post').exists())  # Проверяем, что пост был создан
